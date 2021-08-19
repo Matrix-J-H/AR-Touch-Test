1208fa79308bf07fa2f3e3f06cd4a01548af9cb1
Matrix J <33464122+Matrix-J-H@users.noreply.github.com>
2021-08-19 01:10:30 +0100

    Final commit
    Done

---
+++ ShatteredBridge/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{   
+    public GameObject startPoint;
+    private int count;
+    public Text scoreText;
+    public Text scoreTextFinal;
+    public GameObject TimeCounter;
+    public GameObject WinMessage;
+
+    private void Awake()
+    {
+        WinMessage.SetActive(false);
+    }
+
+    private void Start()
+    {
+        count = 0;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Goal":
+                StartGoalSequence();
+                break;
+            case "Trap":
+                Restart();
+                GetComponent<PlayerController>().enabled = true;
+                break;
+            case "PickUp":
+                other.gameObject.SetActive(false);
+                count++;
+                SetScoreText();
+                break;
+        }
+    }
+
+    void StartGoalSequence()
+    {   
+        SetWinMessage();
+        GetComponent<PlayerController>().enabled = false;
+        TimerController timerController = TimeCounter.GetComponent<TimerController>();
+        timerController.EndTimer();
+        //Invoke("ReloadLevel", 1f);
+    }
+
+    private void Restart()
+    {   
+        GetComponent<PlayerController>().enabled = false;
+        startPoint.GetComponent<SpawnPlayer>().firstSpawn();
+    }
+
+    public void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    private void SetScoreText()
+    {
+        scoreText.text = "Score:" + count.ToString();
+    }
+
+    private void SetWinMessage()
+    {
+        WinMessage.SetActive(true);
+        scoreTextFinal.text = "You got score of " + count + "!";
+    }
+    
+}
+
---
+++ ShatteredBridge/Assets/Scripts/TimerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public static TimerController instance;
+
+    public Text timeCounter;
+
+    private TimeSpan timePlayed;
+    private bool timerRunning;
+
+    private float timePassed;
+
+    private void Awake() 
+    {
+        instance = this;
+    }
+    
+    private void Start() 
+    {
+        timeCounter.text = "Time: 00:00";
+        BeginTimer();
+    }
+
+    private void BeginTimer()
+    {
+        timerRunning = true;
+        timePassed = 0f;
+
+        StartCoroutine(UpdateTimer());
+    }
+
+    public void EndTimer()
+    {
+        timerRunning = false;
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while(timerRunning)
+        {
+            timePassed += Time.deltaTime;
+            timePlayed = TimeSpan.FromSeconds(timePassed);
+            string timePlayedTxt = "Time: " + timePlayed.ToString("mm':'ss");
+            timeCounter.text = timePlayedTxt;
+
+            yield return null;
+        }
+    }
+}
